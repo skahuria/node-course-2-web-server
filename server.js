b6888dbbf62d9febfd9f08d4d20726453d7665d7
skahuria <stephenkahuria@ymail.com>
2018-05-10 15:17:11 +0300

    add project page

--- server.js
+++ server.js
@@ -29,10 +29,15 @@ app.get('/', (req, res)=> {   //route handler registration
 });
 
 app.get('/about', (req, res) =>{
-  //res.send('About Page');
   res.render('about.hbs', {
     pageTitle: 'About Page',
 
+  });
+});
+
+app.get('/projects', (req, res) =>{
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
   });
 });
 
